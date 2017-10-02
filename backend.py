e30e44bbbd219acdd00a604121584d39959145f4
Marius Stan <marius.stan@sympatico.ca>
2017-10-02 19:08:59 -0400

    Initial commit

---
+++ backend.py
@@ -0,0 +1,10 @@
+from crawler import crawler
+
+bot = crawler(None, "urls.txt")
+bot.crawl(depth=1)
+
+inverted_index = bot.get_inverted_index()
+print inverted_index
+
+resolved_inverted_index = bot.get_resovled_inverted_index()
+print resolved_inverted_index
